8c365b655bc8008cc94a2b50c26906562e0725df
Andrew Ayer <andrew@sslmate.com>
2024-01-05 20:12:14 -0500

    Add database indexes

---
+++ schema/002.sql
@@ -0,0 +1,3 @@
+CREATE INDEX http_request_by_test_id ON http_request (test_id);
+CREATE INDEX dns_request_by_test_id ON dns_request (test_id);
+CREATE INDEX smtp_request_by_test_id ON smtp_request (test_id);
